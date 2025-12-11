00109458e82aea41fc93c991222073470c885d63
тҽαɱ ɾιყα ɱυʂιƈ <ksdigitaladsstudio@gmail.com>
2025-12-11 09:27:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/TEAMRIYA/RiyaMusic",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
